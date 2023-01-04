23a151e6e1e16ee80b134c63b727d87903970fa4
Barshaa21 <117267581+Barshaa21@users.noreply.github.com>
2023-01-04 17:17:09 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,73 +5,58 @@ import { useState } from 'react';
 import Remove from './Mycomponents/Remove';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [con, setCon] = useState({ desc: '', title: '',edit: null });
 
-  const [mainState, setmainState] = useState(
-    {
-      todos: [],
-      title: '',
-      desc: '',
-      toggle: true,
-      edit: null
-    })
 
   const submit = (e) => {
     e.preventDefault();
-    if (!mainState.title || !mainState.desc) { alert('Title or description cannot be blank') }
+    if (!con.title || !con.desc) { alert('title or description cannot be blank') }
     else {
-      addTodo(mainState.title, mainState.desc);
-      setmainState({...mainState, title: '' })
-      setmainState({...mainState, desc: '' })
+      addTodo(con.title, con.desc);
+      setCon({ ...con, title: '',desc:'' })
     }
   }
 
-
-  const addTodo = (title, desc) => {
-    if (mainState.title && !mainState.toggle) {
-      setmainState({...mainState,
-        todos: (mainState.todos.map((todo) => {
-          if (mainState.todo.id === mainState.edit) { return { ...todo, title: mainState.title, desc: mainState.desc } }
-          return todo;
-        }))
+  const addTodo = (title, desc, id) => {
+    if (con.edit) {
+      setTodos(todos.map((todo) => {
+        if (todo.id === con.edit) { return { ...todo, title: con.title, desc: con.desc } }
+        return todo;
       })
-      setmainState({...mainState,title: '' })
-      setmainState({...mainState,desc: '' })
-      setmainState({...mainState,toggle: true });
-      setmainState({...mainState,edit: null })
+      )
+      setCon({ ...con, title: '',desc:'',edit:null })
     }
     else {
       let id = new Date().getTime().toString();
       console.log(id);
       const myTodo = {
         id: id,
-        title: mainState.title,
-        desc: mainState.desc,
+        title: con.title,
+        desc: con.desc,
       }
-      setmainState({...mainState, todos:[...mainState.todos, myTodo]})
+      setTodos([...todos, myTodo]);
+      console.log(myTodo);
+
     }
   }
-
-  const editTodo = (id, title, desc) => {
-    let editedItem = mainState.todos.find((todo) => {
+  const editTodo = (id) => {
+    let editedItem = todos.find((todo) => {
       return todo.id === id
     });
-    setmainState({ ...mainState,title: editedItem.title })
-    setmainState({ ...mainState,desc: editedItem.desc })
-    setmainState({...mainState, toggle: false });
-    setmainState({...mainState, edit: id })
+    setCon({ ...con, title: editedItem.title,desc:editedItem.desc,edit:id  })
   }
 
   const deleteall = (todos) => {
-    setmainState({...mainState, todos: [] })
+    setTodos([]);
   }
-
   const onDelete = (todo) => {
-    setmainState({...mainState,
-      todos: (mainState.todos.filter((e) => {
-        return e !== todo;
-      }))
-    })}
-
+    console.log('i am deleted', todo);
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }
+    ));
+  }
 
   return (
     <div className="App">
@@ -79,23 +64,21 @@ function App() {
         {
           <>
             <Header />
-            <div className='div' style={{ color: 'white', paddingTop: '20px', }}>
+               <div className='div' style={{ color: 'white', paddingTop: '20px', }}>
               <form onSubmit={submit}>
                 <div className="container mb-3 border border-dark" style={{ width: '550px', height: '100px', backgroundColor: 'black' }}>
                   <label htmlFor="Title" className="form-label">Todo Title</label>
-                  <input type="text" value={mainState.title} onChange={(e) => setmainState({...mainState, title: e.target.value })} className="form-control" id="tiltle" />
+                  <input type="text" value={con.title} onChange={(e) => setCon({ ...con, title: e.target.value })} className="form-control" />
                 </div>
                 <div className="mb-3 container border border-dark " style={{ width: '550px', height: '100px', backgroundColor: "black" }} >
-                  <label htmlFor="Description<" className="form-label">Description</label>
-                  <input type="text" value={mainState.desc} onChange={(e) => setmainState({ ...mainState,desc: e.target.value })} className="form-control" id="exampleInputPassword1" />
+                  <label htmlFor="Description" className="form-label">Description</label>
+                  <input type="text" value={con.desc} onChange={(e) => setCon({ ...con, desc: e.target.value })} className="form-control" />
                 </div>
-                {mainState.toggle ? <button type="submit" className="btn btn-primary border border-dark"><span className="bi bi-trash"></span>Add todo</button> :
-                  <button type="submit" className="btn btn-primary border border-dark"><span className="bi bi-trash"></span>Edit todo</button>
-                }
-
+               
+                <button type="submit" className="btn btn-primary border border-dark"><span className="bi bi-trash"></span>Add todo</button>
               </form>
             </div>
-            <Todos todos={mainState.todos} onDelete={onDelete} id={addTodo} editTodo={editTodo} />
+            <Todos todos={todos} onDelete={onDelete} id={addTodo} editTodo={editTodo} />
             <Remove deleteall={deleteall} />
           </>
         }
