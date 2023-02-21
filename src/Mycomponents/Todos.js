8cf4c824bc0622de9b6b0d9cbb4a4d418ed5236c
Barshaa21 <117267581+Barshaa21@users.noreply.github.com>
2023-02-21 20:48:13 +0545

    Update Todos.js

--- src/Mycomponents/Todos.js
+++ src/Mycomponents/Todos.js
@@ -3,8 +3,8 @@ import TodoItem from './TodoItem';
 
 const Todos = (props) => {
   return (
-    <div className='container ' >
-      <h3 className='container text-center my-3 bg-light text-dark rounded border border-dark' style={{ width: '200px' }}>Todos list</h3>
+    <div className='container Todos' >
+      <h2 >Todos list</h2>
       {props.todos.length === 0 ? "No todos to display" : props.todos.map((todo) => {
         return (<div key={todo.id}><TodoItem todos={props.todos} todo={todo} editTodo={props.editTodo} onDelete={props.onDelete} /></div>)
       })}
