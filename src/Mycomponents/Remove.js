526d35a6defbe73c151383d1cced310de64b3860
Barshaa21 <117267581+Barshaa21@users.noreply.github.com>
2023-02-21 20:46:38 +0545

    Update Remove.js

--- src/Mycomponents/Remove.js
+++ src/Mycomponents/Remove.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Remove({deleteall}) {
+export default function Remove({ deleteall }) {
   return (
     <div>
-       <button className="btn btn-primary btn-sm" onClick={() => { deleteall() }}>Delete all</button>
+      <button className="btn" onClick={() => { deleteall() }}>Delete all</button>
     </div>
   )
 }
